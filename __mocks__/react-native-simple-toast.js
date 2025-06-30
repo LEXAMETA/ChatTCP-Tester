d676e702624055b4fbbbf33893ca94735bb73413
LEXAMETA <lexagreeter@gmail.com>
2025-06-30 12:52:26 -0500

    Create react-native-simple-toast.js

---
+++ __mocks__/react-native-simple-toast.js
@@ -0,0 +1,7 @@
+// __mocks__/react-native-simple-toast.js
+export default {
+  show: jest.fn(),
+  showWithGravity: jest.fn(),
+  showWithGravityAndOffset: jest.fn(),
+  // Add any other methods your code calls from this module
+};
