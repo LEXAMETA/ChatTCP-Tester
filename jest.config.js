5eba1b461cc5c3b99448fc3c78207ca77d94fb1f
LEXAMETA <lexagreeter@gmail.com>
2025-06-30 13:01:01 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,10 @@
+// jest.config.js
 module.exports = {
   preset: 'jest-expo',
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|cui-llama.rn|expo-router)'
   ],
   setupFilesAfterEnv: ['@testing-library/react-native/extend-expect'],
-  testEnvironment: 'node'
+  // REMOVE OR COMMENT THIS LINE:
+  // testEnvironment: 'node'
 };
